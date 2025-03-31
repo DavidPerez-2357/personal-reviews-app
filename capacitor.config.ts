adc418fa41624716b8a5b103b492fcce96cb0270
David Perez <dperezfernandez@safareyes.es>
2025-03-31 13:39:39 +0200

    ➕ Add splash screen plugin

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,24 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'personal-review-app',
-  webDir: 'dist'
+  appId: 'com.ionic.personalreviews',
+  appName: 'Personal Reviews',
+  webDir: 'dist',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      launchFadeOutDuration: 300,
+      backgroundColor: "#222831FF",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: true,
+      androidSpinnerStyle: "large",
+      iosSpinnerStyle: "small",
+      spinnerColor: "#00ADB5FF",
+      splashFullScreen: true,
+      splashImmersive: true,
+    },
+  },
 };
 
 export default config;
