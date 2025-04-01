fbfa4c94fb7781db43356a8cb384688b0e8f9cc0
david <ticotico6365@gmail.com>
2025-04-01 15:27:07 +0200

    ➕ Add sqlite

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,24 +1,27 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type {CapacitorConfig} from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.ionic.personalreviews',
-  appName: 'Personal Reviews',
-  webDir: 'dist',
-  plugins: {
-    SplashScreen: {
-      launchShowDuration: 3000,
-      launchAutoHide: true,
-      launchFadeOutDuration: 300,
-      backgroundColor: "#222831FF",
-      androidScaleType: "CENTER_CROP",
-      showSpinner: true,
-      androidSpinnerStyle: "large",
-      iosSpinnerStyle: "small",
-      spinnerColor: "#00ADB5FF",
-      splashFullScreen: true,
-      splashImmersive: true,
+    appId: 'io.ionic.starter',
+    appName: 'personal-review-app',
+    webDir: 'dist',
+    plugins: {
+        CapacitorSQLite: {
+            androidDatabaseLocation: "default",
+        },
+        SplashScreen: {
+            launchShowDuration: 3000,
+            launchAutoHide: true,
+            launchFadeOutDuration: 300,
+            backgroundColor: "#222831FF",
+            androidScaleType: "CENTER_CROP",
+            showSpinner: true,
+            androidSpinnerStyle: "large",
+            iosSpinnerStyle: "small",
+            spinnerColor: "#00ADB5FF",
+            splashFullScreen: true,
+            splashImmersive: true,
+        },
     },
-  },
 };
 
 export default config;
