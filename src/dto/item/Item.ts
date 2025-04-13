07b566b3616dc2e8bdd14cd73a05ee953fb71d71
David Perez <dperezfernandez@safareyes.es>
2025-04-13 20:34:23 +0200

    ✨ Create item option dto

--- src/dto/item/Item.ts
+++ src/dto/item/Item.ts
@@ -1,6 +1,13 @@
 export interface Item {
+    id: number;
     name: string;
     image: string | null;
     rating: number;
+    category_id: number;
+}
+
+export interface ItemOption {
+    id: number;
+    name: string;
     category_id: number;
 }
