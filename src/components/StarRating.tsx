636b3840c2c9031c0ab7c9b408b1db700ac84c6d
David Perez <dperezfernandez@safareyes.es>
2025-04-13 20:35:04 +0200

    ✨ Create star rating component

---
+++ src/components/StarRating.tsx
@@ -0,0 +1,23 @@
+import StarCheckbox from "@/components/StarCheckbox";
+
+type StarRatingProps = {
+    size: number;
+    rating: number;
+    setRating: (rating: number) => void;
+    canEdit?: boolean;
+    classes?: string;
+}
+
+const StarRating = ({ size, rating, setRating, canEdit = true, classes = "" }: StarRatingProps) => {
+    return (
+        <div className={`flex gap-1` + (canEdit ? " cursor-pointer " : " select-none pointer-events-none ") + classes}>
+            <StarCheckbox index={1} checked={rating >= 1} size={size} rating={rating} setRating={setRating} />
+            <StarCheckbox index={2} checked={rating >= 2} size={size} rating={rating} setRating={setRating} />
+            <StarCheckbox index={3} checked={rating >= 3} size={size} rating={rating} setRating={setRating} />
+            <StarCheckbox index={4} checked={rating >= 4} size={size} rating={rating} setRating={setRating} />
+            <StarCheckbox index={5} checked={rating >= 5} size={size} rating={rating} setRating={setRating} />
+        </div>
+    )
+}
+
+export default StarRating;
