2d65c52b836a93777dcccfd2c8786045830f5351
David Perez <dperezfernandez@safareyes.es>
2025-04-13 20:33:21 +0200

    🚧 Add temporal loading on app

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,67 @@
 import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import Home from './pages/Home';
 
 import '@styles/global.css';
 
 import FooterTabBar from './components/FooterTabBar';
 
 import { SafeArea } from '@capacitor-community/safe-area';
-import { useEffect } from 'react';
-import { StatusBar } from '@capacitor/status-bar';
+import { useEffect, useState } from 'react';
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { initDB } from '@/database-service';
+import { insertTestCategories } from './services/category-service';
+import { insertTestItems } from './services/item-service';
+
+library.add(fas);
 
 setupIonicReact();
 
 const App: React.FC = () => {
-  useEffect(() => {
-    SafeArea.enable({
-      config: {
-        customColorsForSystemBars: true,
-        statusBarColor: '#00000000', // transparent
-        statusBarContent: 'light',
-        navigationBarColor: '#00000000', // transparent
-        navigationBarContent: 'light',
-      },
-    });
-    
+  const [dbReady, setDbReady] = useState(false);
 
-    StatusBar.setOverlaysWebView({ overlay: true });
-    StatusBar.setBackgroundColor({ color: '#00000000' });
+  useEffect(() => {
+    const init = async () => {
+      try {
+        await SafeArea.enable({
+          config: {
+            customColorsForSystemBars: true,
+            statusBarColor: '#00000000',
+            statusBarContent: 'light',
+            navigationBarColor: '#00000000',
+            navigationBarContent: 'light',
+          },
+        });
+
+        await initDB(); // Esperamos a que termine de inicializar la DB
+        /* await insertTestCategories(); // Insertamos categorías de prueba
+        await insertTestItems(); // Insertamos ítems de prueba */
+        console.log('Base de datos inicializada y datos de prueba insertados');
+      } catch (error) {
+        console.error('Error during app init', error);
+      } finally {
+        setDbReady(true);
+      }
+    };
+
+    init();
   }, []);
+
+  if (!dbReady) {
+    return (
+      <IonApp>
+        <div style={{ padding: '2rem', textAlign: 'center' }}>Inicializando base de datos...</div>
+      </IonApp>
+    );
+  }
 
   return (
     <IonApp>
       <IonReactRouter>
         <IonRouterOutlet>
           <Route path="/app" component={FooterTabBar} />
-          
           <Route exact path="/">
             <Redirect to="/app" />
           </Route>
@@ -44,7 +69,6 @@ const App: React.FC = () => {
       </IonReactRouter>
     </IonApp>
   );
-  
 };
 
 export default App;
