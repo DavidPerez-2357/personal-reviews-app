9a12d1be4ef1728107ca5696f48a520b62cd7ae8
David Perez <dperezfernandez@safareyes.es>
2025-04-25 20:02:13 +0200

    ♻️ Change routes relatives to absolutes

--- i18n.ts
+++ i18n.ts
@@ -2,8 +2,8 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from 'i18next-browser-languagedetector';
-import english from "./public/locales/en/translation.json";
-import spanish from "./public/locales/es/translation.json";
+import english from "@shared/i18n/en/translation.json";
+import spanish from "@shared/i18n/es/translation.json";
 
 i18n
 .use(LanguageDetector)
