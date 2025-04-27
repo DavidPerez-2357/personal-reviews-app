59cfe0205e32c462ca309d472a60b5c311baf4cb
david <ticotico6365@gmail.com>
2025-04-27 18:13:09 +0200

    ♻️ Change ubication

---
+++ src/shared/dto/Item.ts
@@ -0,0 +1,19 @@
+export interface Item {
+    id: number;
+    name: string;
+    image: string | null;
+    category_id: number;
+}
+
+export interface ItemOption {
+    id: number;
+    name: string;
+    category_id: number;
+    parent_category_id: number | null;
+    parent_category_icon: string;
+}
+
+export interface Origin {
+    origin_id: number;
+    item_id: number;
+}
---
+++ src/shared/dto/Review.ts
@@ -0,0 +1,22 @@
+export interface Review {
+    id: number;
+    rating: number;
+    comment: string | null; 
+    item_id: number;
+}
+
+export interface ReviewFull {
+    id: number;
+    comment: string;
+    rating: number;
+    created_at: string;
+    updated_at: string;
+    images: string[];
+    category: string;
+    item: string;
+}
+
+export interface ReviewImage {
+    image: string;
+    review_id: number;
+}
