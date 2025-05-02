11ef4d4663be72d7e4853aeaaf75a0124447d6e6
David Perez <dperezfernandez@safareyes.es>
2025-05-02 19:42:49 +0200

    ♻️ Refactor star rating and if cant edit deactivate the animation

--- src/shared/components/StarRating.tsx
+++ src/shared/components/StarRating.tsx
@@ -1,23 +1,39 @@
 import StarCheckbox from "@/shared/components/StarCheckbox";
 
 type StarRatingProps = {
-    size: number;
-    rating: number;
-    setRating: (rating: number) => void;
-    canEdit?: boolean;
-    classes?: string;
-}
+  size: number;
+  rating: number;
+  setRating: (rating: number) => void;
+  canEdit?: boolean;
+  classes?: string;
+};
 
-const StarRating = ({ size, rating, setRating, canEdit = true, classes = "" }: StarRatingProps) => {
-    return (
-        <div className={`flex gap-1` + (canEdit ? " cursor-pointer " : " select-none pointer-events-none ") + classes}>
-            <StarCheckbox index={1} checked={rating >= 1} size={size} rating={rating} setRating={setRating} />
-            <StarCheckbox index={2} checked={rating >= 2} size={size} rating={rating} setRating={setRating} />
-            <StarCheckbox index={3} checked={rating >= 3} size={size} rating={rating} setRating={setRating} />
-            <StarCheckbox index={4} checked={rating >= 4} size={size} rating={rating} setRating={setRating} />
-            <StarCheckbox index={5} checked={rating >= 5} size={size} rating={rating} setRating={setRating} />
-        </div>
-    )
-}
+const StarRating = ({
+  size,
+  rating,
+  setRating,
+  canEdit = true,
+  classes = "",
+}: StarRatingProps) => {
+  return (
+    <div
+      className={`flex gap-1 ${
+        canEdit ? "cursor-pointer" : "select-none pointer-events-none"
+      } ${classes}`}
+    >
+      {[1, 2, 3, 4, 5].map((index) => (
+        <StarCheckbox
+          key={index}
+          index={index}
+          checked={rating >= index}
+          size={size}
+          rating={rating}
+          setRating={setRating}
+          canEdit={canEdit}
+        />
+      ))}
+    </div>
+  );
+};
 
 export default StarRating;
