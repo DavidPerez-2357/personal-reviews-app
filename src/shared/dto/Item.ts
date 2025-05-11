05a4e59ceffb5958ef9e35f03f63d05a84e0db8f
david <ticotico6365@gmail.com>
2025-05-11 12:37:20 +0200

    ✨ Add ItemFull interface to enhance item data structure

--- src/shared/dto/Item.ts
+++ src/shared/dto/Item.ts
@@ -17,3 +17,15 @@ export interface Origin {
     origin_id: number;
     item_id: number;
 }
+
+export interface ItemFull {
+    id: number;
+    name: string;
+    image: string | null;
+    created_at: string;
+    updated_at: string;
+    category_id: number;
+    category_name: string;
+    category_icon: string;
+    category_color: string;
+}
