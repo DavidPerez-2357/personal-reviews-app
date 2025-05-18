bcf178709037198167d6e078464a1aa653bddd9a
david <ticotico6365@gmail.com>
2025-05-18 10:36:02 +0200

    ✨ Refactor ViewItem component to enhance item display and reviews functionality

--- src/app/view-item/ViewItem.tsx
+++ src/app/view-item/ViewItem.tsx
@@ -1,107 +1,272 @@
-import React, { useEffect } from 'react';
-import { IonContent, IonPage, IonGrid, IonRow, IonCol, IonBackButton } from '@ionic/react';
-import { useParams } from 'react-router';
-import { getItems } from '@/shared/services/item-service';
-import { Item, ItemFull } from '@/shared/dto/Item';
-import { Category } from '@/shared/dto/Category';
+import React, { useEffect } from "react";
+import {
+  IonContent,
+  IonPage,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonBackButton,
+  IonHeader,
+  IonButton,
+} from "@ionic/react";
+import { useParams } from "react-router";
+import { getItems } from "@/shared/services/item-service";
+import { ItemDisplay, ItemFull, OriginDisplay } from "@/shared/dto/Item";
+import { ChevronDown, EllipsisVertical } from "lucide-react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { IconName } from "@fortawesome/fontawesome-svg-core";
+import { CategoryColors } from "@/shared/enums/colors";
+import StarRating from "@/shared/components/StarRating";
+import { Review } from "@/shared/dto/Review";
+import TimelineEntry from "./components/TimeLineEntry";
+import ItemCard from "./components/ItemCard";
+import OriginCard from "./components/OriginCard";
 
 export const ViewItem = () => {
+  const { id } = useParams<{ id: string }>();
 
-    const { id } = useParams<{ id: string }>();
+  const [showTimeline, setShowTimeline] = React.useState(false);
 
-    const [item, setItem] = React.useState<Item | null>(null);
+  const [item, setItem] = React.useState<ItemFull>({
+    id: 0,
+    name: "",
+    image: null,
+    created_at: "",
+    updated_at: "",
+    category_id: 0,
+    category_name: "",
+    category_icon: "",
+    category_color: "",
+  });
 
-    async function initializeData() {
+  const [reviews, setReviews] = React.useState<Review[]>([]);
+  const [itemsOfOrigin, setItemsOfOrigin] = React.useState<OriginDisplay[]>([]);
+
+  useEffect(() => {
+    const initializeData = async () => {
       try {
-        // const itemsFromDB = await getItems();
+        // const mainItemDetailsFromDB = await getItem(id);
 
-        const categories: Category[] = [
-            { id: 1, name: "Electrónica", type: 1, color: "red", icon: "computer", parent_id: null },
-            { id: 2, name: "Ropa", type: 2, color: "green", icon: "shirt", parent_id: null},
-            { id: 3, name: "Hogar", type: 3, color: "blue", icon: "house", parent_id: null},
-            { id: 4, name: "Juguetes", type: 4, color: "yellow", icon: "chess-knight", parent_id: null},
-            { id: 5, name: "Deportes", type: 5, color: "gray", icon: "football", parent_id: null},
-            { id: 6, name: "Libros", type: 6, color: "darkgray", icon: "book", parent_id: null},
-            { id: 7, name: "Salud", type: 7, color: "turquoise", icon: "staff-snake", parent_id: null},
-            { id: 8, name: "Belleza", type: 8, color: "purple", icon: "bath", parent_id: null},
-            { id: 9, name: "Automóviles", type: 9, color: "red", icon: "car", parent_id: null},
-            { id: 10, name: "Oficina", type: 10, color: "darkgray", icon: "file", parent_id: null},
-            // Subcategorías
-            { id: 11, name: "Smartphones", type: 1, color: "darkgray", icon: "mobile", parent_id: 1},
-            { id: 12, name: "Portátiles", type: 1, color: "purple", icon: "laptop", parent_id: 1},
-            { id: 13, name: "Zapatillas", type: 2, color: "green", icon: "shoe-prints", parent_id: 2},
-            { id: 14, name: "Vestidos", type: 2, color: "red", icon: "person-dress", parent_id: 2},
-            { id: 15, name: "Muebles", type: 3, color: "turquoise", icon: "couch", parent_id: 3},
-            { id: 16, name: "Cocina", type: 3, color: "red", icon: "kitchen-set", parent_id: 3},
-            { id: 17, name: "Libros Infantiles", type: 6, color: "purple", icon: "book-skull", parent_id: 6},
-            { id: 18, name: "Maquillaje", type: 8, color: "green", icon: "soap", parent_id: 8},
-            { id: 19, name: "Suplementos", type: 7, color: "blue", icon: "prescription-bottle", parent_id: 7},
-            { id: 20, name: "Sillas", type: 3, color: "yellow", icon: "chair", parent_id: 3},
-        ];
-
-    const items: Item[] = [
-        { id: 1, name: "iPhone 13", image: "iphone-13.jpg", category_id: 11 },
-        { id: 2, name: "MacBook Air M1", image: "macbook-air.jpg", category_id: 12 },
-        { id: 3, name: "Nike Air Force 1", image: "nike-airforce.jpg", category_id: 13 },
-        { id: 4, name: "Zapatillas Adidas Ultraboost", image: "adidas-ultraboost.jpg", category_id: 13 },
-        { id: 5, name: "Silla de oficina ergonómica", image: "silla-oficina.jpg",  category_id: 10 },
-        { id: 6, name: "Cafetera Nespresso", image: "cafetera-nespresso.jpg", category_id: 15 },
-        { id: 7, name: "Kindle Paperwhite", image: "kindle-paperwhite.jpg", category_id: 6 },
-        { id: 8, name: "Mochila para portátil", image: "mochila-portatil.jpg", category_id: 10 },
-        { id: 9, name: "Reloj Garmin Forerunner", image: "garmin-forerunner.jpg", category_id: 5 },
-        { id: 10, name: "Sony WH-1000XM4", image: "sony-headphones.jpg", category_id: 1 },
-        { id: 11, name: "Teclado mecánico Logitech", image: "logitech-teclado.jpg", category_id: 1 },
-        { id: 12, name: "Guitarra Fender Stratocaster", image: "fender-guitarra.jpg", category_id: 4 },
-        { id: 13, name: "Mueble modular para oficina", image: "mueble-oficina.jpg", category_id: 15 },
-        { id: 14, name: "Silla gaming DXRacer", image: "dxracer-silla.jpg", category_id: 10 },
-        { id: 15, name: "Suplemento Omega-3", image: "suplemento-omega3.jpg", category_id: 19 },
-        { id: 16, name: "Lámpara de escritorio LED", image: "lampara-escritorio.jpg", category_id: 10 },
-        { id: 17, name: "Smartwatch Samsung Galaxy", image: "samsung-smartwatch.jpg", category_id: 9 },
-        { id: 18, name: "Juego de platos Corelle", image: "platos-corelle.jpg", category_id: 3 },
-        { id: 19, name: "Silla ergonómica para oficina", image: "silla-ergonomica.jpg", category_id: 15 },
-        { id: 20, name: "Altavoces Bose SoundLink", image: "bose-altavoces.jpg", category_id: 1 },
-        { id: 21, name: "Auriculares Beats Studio", image: "beats-audifonos.jpg", category_id: 1 },
-        { id: 22, name: "Sofá 3 plazas", image: "sofa-3-plazas.jpg", category_id: 15 },
-        { id: 23, name: "Plancha de vapor Philips", image: "plancha-philips.jpg", category_id: 14 },
-        { id: 24, name: "Cámara GoPro Hero 10", image: "gopro-hero10.jpg", category_id: 1 },
-        { id: 25, name: "Pantalla LED 4K LG", image: "pantalla-lg.jpg", category_id: 1 }
-        ];  
-
-        const itemFromDB: ItemFull = {
+        const mainItemDetailsFromDB: ItemFull = {
+          id: 1,
+          name: "Apple",
+          image: "https://desarrolloweb.com/media/761/imagenes-html.jpg",
+          created_at: "2023-10-01",
+          updated_at: "2023-10-01",
+          category_id: 11,
+          category_name: "Brand",
+          category_icon: "apple-whole",
+          category_color: "red",
+        };
+        const itemReviews: Review[] = [
+          {
             id: 1,
-            name: "iPhone 13",
-            image: "iphone-13.jpg",
+            rating: 5,
+            comment: "Excelente producto",
+            item_id: 1,
             created_at: "2023-10-01",
             updated_at: "2023-10-01",
-            category_id: 11,
-            category_name: "Smartphones",
-            category_icon: "mobile",
-            category_color: "darkgray"
-        }
+          },
+          {
+            id: 2,
+            rating: 4,
+            comment: "Muy bueno, pero caro",
+            item_id: 1,
+            created_at: "2023-10-02",
+            updated_at: "2023-10-02",
+          },
+          {
+            id: 3,
+            rating: 3,
+            comment: "Rinde bien, pero no es lo que esperaba",
+            item_id: 1,
+            created_at: "2023-10-03",
+            updated_at: "2023-10-03",
+          },
+          {
+            id: 4,
+            rating: 2,
+            comment: "No me gustó, se calienta mucho",
+            item_id: 1,
+            created_at: "2023-10-04",
+            updated_at: "2023-10-04",
+          },
+          {
+            id: 5,
+            rating: 1,
+            comment: "Malísimo, no lo compren",
+            item_id: 1,
+            created_at: "2023-10-05",
+            updated_at: "2023-10-05",
+          },
+        ];
 
+        const itemsOfOrigin: OriginDisplay[] = [
+          {
+            id: 2,
+            name: "iPhone 14 Pro",
+            category_name: "Brand",
+            category_icon: "mobile-alt",
+            category_color: "red",
+            average_rating: 4.5,
+            average_rating_all_items: 0,
+            number_of_ratings: 120,
+            date_last_review: "2023-10-10",
+          },
+          {
+            id: 3,
+            name: "iPhone 13",
+            category_name: "Brand",
+            category_icon: "mobile-alt",
+            category_color: "red",
+            average_rating: 4.2,
+            average_rating_all_items: 4.3,
+            number_of_ratings: 98,
+            date_last_review: "2023-09-15",
+          },
+          {
+            id: 4,
+            name: "iPhone SE",
+            category_name: "Brand",
+            category_icon: "mobile-alt",
+            category_color: "red",
+            average_rating: 3.8,
+            average_rating_all_items: 4.3,
+            number_of_ratings: 45,
+            date_last_review: "2023-08-20",
+          },
+        ];
 
-        setItem(itemFromDB);
+        setItem(mainItemDetailsFromDB);
+        setReviews(
+          itemReviews.sort(
+            (a, b) =>
+              new Date(b.created_at).getTime() -
+              new Date(a.created_at).getTime()
+          )
+        );
+        setItemsOfOrigin(itemsOfOrigin);
       } catch (err) {
         console.error("Error initializing data:", err);
       }
-    }
+    };
     initializeData();
+  }, []);
 
-    return (
-        <IonPage className="ion-padding review-page">
-            <IonContent className="ion-padding">
-                <IonGrid>
-                    <IonRow className="ion-justify-content-center ion-align-items-center">
-                        <IonCol size="12" className="ion-text-center">
-                            <IonBackButton defaultHref="/app/items" color="tertiary" />
-                            <h1>View Item</h1>
-                            <p>Item ID: {id}</p>
-                        </IonCol>
-                    </IonRow>
-                </IonGrid>
-            </IonContent>
-        </IonPage>
-    );
+  return (
+    <IonPage>
+      <IonContent>
+        <IonRow className="flex justify-between items-center ion-padding">
+          <IonBackButton defaultHref="/app/items" />
+          <EllipsisVertical size={30} />
+        </IonRow>
+        <div className="flex flex-col gap-6">
+          <div
+            className="h-48 bg-cover bg-center flex items-center justify-center"
+            style={{
+              backgroundImage: `url(${item.image})`, // item.image puede ser null, CSS lo manejará
+            }}
+          >
+            <IonCol className="flex items-center h-1/2 bg-[#222831]/80 ion-padding gap-3">
+              <div
+                className={`p-5 rounded-lg w-16 h-16 flex items-center justify-center`}
+                style={{
+                  backgroundColor: CategoryColors[item.category_color],
+                }}
+              >
+                <FontAwesomeIcon
+                  icon={item.category_icon as IconName}
+                  className="fa-2xl text-[var(--ion-color-primary-contrast)]"
+                />
+              </div>
+              <span className="text-2xl font-bold break-words text-[var(--ion-color-primary-contrast)]">
+                {item.name}
+              </span>
+            </IonCol>
+          </div>
+          <div className="ion-padding flex flex-col gap-3 justify-center items-center">
+            {reviews.length > 0 && (
+              <>
+                <StarRating
+                  size={50}
+                  rating={reviews[0].rating}
+                  setRating={() => {}}
+                />
+                <span className="text-[var(--ion-text-color)] text-lg">
+                  {reviews[0].comment}
+                </span>
+              </>
+            )}
+          </div>
+          <IonButton
+            color="tertiary"
+            expand="block"
+            className="bg-primary mx-5"
+            routerLink="/app/reviews/create"
+          >
+            Crear Reseña
+          </IonButton>
+          {reviews.length > 1 && (
+            <div className="px-5 flex flex-col gap-4">
+              <div
+                className="flex justify-between items-center cursor-pointer"
+                onClick={() => setShowTimeline(!showTimeline)}
+              >
+                <span className="font-bold text-[var(--ion-text-color)] text-3xl">
+                  Línea del tiempo
+                </span>
+                <div className="flex items-center gap-2">
+                  <span className="text-[var(--ion-color-tertiary-contrast)] text-sm p-1 rounded-full bg-[var(--ion-color-tertiary)] flex items-center justify-center w-6 h-6">
+                    {reviews.length}
+                  </span>
+                  <ChevronDown
+                    className={`transition-transform ${
+                      showTimeline ? "rotate-180" : ""
+                    }`}
+                  />
+                </div>
+              </div>
+              {showTimeline && (
+                <div
+                  className={`overflow-hidden transition-all duration-1000 ease-in-out ${
+                    showTimeline ? "max-h-screen" : "max-h-0"
+                  }`}
+                >
+                  <div className="relative flex flex-col justify-center mx-10 pt-4">
+                    {reviews.map((entry, index) => (
+                      <TimelineEntry
+                        key={index}
+                        entry={{
+                          date: entry.created_at,
+                          rating: entry.rating,
+                          comment: entry.comment,
+                        }}
+                      />
+                    ))}
+                    {/* Extending the timeline line to the bottom */}
+                    <div className="absolute left-0 top-0 w-0.5 h-full bg-[var(--ion-color-tertiary)]"></div>
+                  </div>
+                </div>
+              )}
+            </div>
+          )}
+
+          <div className="px-5 flex flex-col gap-4">
+            <span className="font-bold text-[var(--ion-text-color)] text-3xl">
+              Elementos
+            </span>
+            <IonGrid className="gap-4">
+              <div className="flex flex-col gap-7">
+                {itemsOfOrigin.map((item) => (
+                  <ItemCard item={item} />
+                ))}
+              </div>
+            </IonGrid>
+          </div>
+        </div>
+      </IonContent>
+    </IonPage>
+  );
 };
 
 export default ViewItem;
