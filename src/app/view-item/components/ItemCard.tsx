28c5ad732f323e332cfc9da24ffaa5a3ee494c35
david <ticotico6365@gmail.com>
2025-05-26 18:07:13 +0200

    ♻️ Refactor ItemCard component for improved layout and styling consistency

--- src/app/view-item/components/ItemCard.tsx
+++ src/app/view-item/components/ItemCard.tsx
@@ -5,10 +5,16 @@ import { IconName } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { IonButton, IonCard } from "@ionic/react";
 import { SquarePen } from "lucide-react";
+import "../styles/itemCard.css";
 
 const ItemCard = ({ item }: { item: ItemDisplay }) => {
+
   return (
-    <IonCard className="bg-[var(--ion-color-secondary)] p-4 flex items-center gap-4">
+    <IonCard
+      button={true}
+      className="bg-[var(--ion-color-secondary)] px-4 py-5 flex-row gap-4 items-center cursor-pointer"
+      routerLink={`/app/items/${item.id}/viewItem`}
+    >
       <div
         className="flex items-center justify-center w-16 h-16 rounded-md"
         style={{ backgroundColor: CategoryColors[item.category_color] }}
@@ -20,7 +26,7 @@ const ItemCard = ({ item }: { item: ItemDisplay }) => {
       </div>
 
       <div className="flex flex-col">
-        <span className="text-lg text-[var(--ion-color-primary-contrast)]">
+        <span className="text-lg text-[var(--ion-text-color)] break-all w-5/6">
           {item.name}
         </span>
         <div className="flex items-center gap-2">
@@ -29,18 +35,18 @@ const ItemCard = ({ item }: { item: ItemDisplay }) => {
             rating={Math.round(item.average_rating)}
             setRating={() => {}}
           />
-          <span className="flex items-center justify-center rounded-full w-6 h-6 p-2 text-[var(--ion-color-secondary)] bg-[var(--ion-color-secondary-contrast)]">
+          <span className="font-bold text-xs text-center rounded-full p-1 min-h-6 min-w-6 text-[var(--ion-color-secondary)] bg-[var(--ion-color-secondary-contrast)]">
             {item.number_of_ratings}
           </span>
         </div>
       </div>
-      <div className="absolute top-2 right-2">
+      <div className="absolute top-3 right-3">
         <IonButton
           color={"tertiary"}
           className="aspect-square edit-button"
           routerLink={`/app/item/${item.id}/edit`}
         >
-          <SquarePen size={20}></SquarePen>
+          <SquarePen size={17}></SquarePen>
         </IonButton>
       </div>
     </IonCard>
